ff496d0067db41fdefb66ab4983cd5c15dee8578
Kwoobin <127706596+Kwoobin@users.noreply.github.com>
2023-04-05 20:22:18 +0900

    #5.3 복습
    년도, 월을 시, 분, 초 가 나타나는 값 위에 표현하기 위해 id가 sg인 h3을 만들고 sg에 값을 주기 위해 함수를 추가해봤다.

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,20 @@
 const clock = document.querySelector("h2#clock");
+const sg = document.querySelector("h3#sg");
+const date = new Date();
+
+function getsg() {
+    const FullYear = date.getFullYear();
+    const Month = String(date.getMonth()).padStart(2, "0");
+    sg.innerText = (`${FullYear}.${Month}`)
+}
 
 function getClock() {
-    const date = new Date();
+    
     const Hours = String(date.getHours()).padStart(2, "0");
     const Minutes = String(date.getMinutes()).padStart(2, "0");
     const Seconds = String(date.getSeconds()).padStart(2, "0");
     clock.innerText = (`${Hours}:${Minutes}:${Seconds}`);
 }
+getsg();
 getClock();
 setInterval(getClock, 1000);
