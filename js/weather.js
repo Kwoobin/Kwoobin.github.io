8f5bd746296b2a21b01604fe203fe32c597fd555
Kwoobin <127706596+Kwoobin@users.noreply.github.com>
2023-04-07 20:08:34 +0900

    #8.1 fetch
    fetch=자바스크립트가 Network를 이용해서 대신 호출해준다.
    fetch=promise=서버에 응답한다.
    fetch를 통해 내용을 추출하면 data를 얻을 수 있고,
    data를 이용해 원하는 값을 찾아서 가져올 수 있다.

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "7aba347704cbf30687b814be932fbf75";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-console.log("You live it", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+fetch(url)
+.then((response) => response.json())
+.then(data => {
+    const weather = document.querySelector("#weather span:first-child");
+    const city = document.querySelector("#weather span:last-child");
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main} ${data.main.temp}`;
+});
+
 }
 function onGeoError() {
     alert("Can't find you. No Weather for you.");
